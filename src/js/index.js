6ec435995edf5ac880d16989d7b4a7e4f58178c0
enzo goulart <enzoo.lu@gmail.com>
2023-04-13 15:03:41 -0300

    ;

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ document.getElementById('confirmar').addEventListener('click', e => {
 function dados() {
     console.log('lsadkfbhv');
     let nome = document.getElementById('name').value;
-    let email = document.getElementById('email').value
+    let email = document.getElementById('email').value;
 
     if (nome.length < 3 || nome == '') {
         Swal.fire(
@@ -15,19 +15,19 @@ function dados() {
                 title: 'Erro:',
                 text: 'Preencha o nome corretamente',
                 icon: 'error'
-            })
+            });
     } else if (email == '' || email.indexOf('@')==-1 || email.indexOf('.')==-1 ) {
         Swal.fire(
             {
                 title: 'Erro:',
                 text: 'Preencha o email corretamente',
                 icon: 'error'
-            })
+            });
     } else {
         Swal.fire(
             {
                 title: 'Dados Salvos',
                 icon: 'success'
-            })
-    }
-}
+            });
+    };
+};
